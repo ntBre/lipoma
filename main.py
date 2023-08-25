7782a995611a4a4a5b1a6aa73ef1ff9df2235451
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-25 06:53:00 -0400

    paste in load_dataset, make main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import json
 import warnings
 from collections import defaultdict
+from typing import Union
 
+from openff.qcsubmit.results import (
+    OptimizationResultCollection,
+    TorsionDriveResultCollection,
+)
 from openff.toolkit.topology import Molecule
 from openff.units.openmm import from_openmm
 from openmm.openmm import HarmonicBondForce
@@ -8,6 +14,31 @@ from openmm.openmm import HarmonicBondForce
 import espaloma as esp
 
 warnings.filterwarnings("ignore", category=UserWarning)
+
+
+# copy pasta from known-issues main.py
+def load_dataset(
+    dataset: str,
+) -> Union[OptimizationResultCollection, TorsionDriveResultCollection]:
+    """Peeks at the first entry of `dataset` to determine its type and
+    then loads it appropriately.
+
+    Raises a `TypeError` if the first entry is neither a `torsion`
+    record nor an `optimization` record.
+    """
+    with open(dataset, "r") as f:
+        j = json.load(f)
+    entries = j["entries"]
+    keys = entries.keys()
+    assert len(keys) == 1  # only handling this case for now
+    key = list(keys)[0]
+    match j["entries"][key][0]["type"]:
+        case "torsion":
+            return TorsionDriveResultCollection.parse_file(dataset)
+        case "optimization":
+            return OptimizationResultCollection.parse_file(dataset)
+        case t:
+            raise TypeError(f"Unknown result collection type: {t}")
 
 
 def to_besmarts(
@@ -49,5 +80,10 @@ def to_besmarts(
     return results
 
 
-molecule = Molecule.from_smiles("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
-print(to_besmarts([molecule]))
+def main():
+    molecule = Molecule.from_smiles("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
+    print(to_besmarts([molecule]))
+
+
+if __name__ == "__main__":
+    main()
