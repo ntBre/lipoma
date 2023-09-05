d1125784aba5671142bbb49aeee24e7c918b367b
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-05 12:11:16 -0400

    factor into functions, re-run to check that bonds.dat == query.dat

--- query.py
+++ query.py
@@ -1,7 +1,9 @@
 # asking espaloma questions about our parameters
 
 import itertools
+import sys
 from collections import defaultdict
+from typing import Tuple
 
 from openff.toolkit import ForceField, Molecule
 from tqdm import tqdm
@@ -26,43 +28,89 @@ total = sum(len(s) for s in ds.entries.values())
 EPS = 10.0
 verbose = False
 
-# map of smirks -> disagreement count
-sage_values = {}
-diffs = defaultdict(list)
-for mol in tqdm(
-    itertools.islice(molecules(ds), None),
-    desc="Comparing parameters",
-    total=total,
-):
-    labels = ff.label_molecules(mol.to_topology())[0]
-    bonds = labels["Bonds"]
-    # angles = labels["Angles"]
-    # torsions = labels["ProperTorsions"]
-    sage_bonds = {}
-    for k, v in bonds.items():
-        i, j = k
-        # t = (i, j, v.length.magnitude, v.k.magnitude)
-        sage_bonds[(i, j)] = (v.k.magnitude, v.smirks)
 
-    _, d = espaloma_label(mol)
-    espaloma_bonds = {}
-    for bond in d["bonds"]:
-        i, j, _, k = bond.from_zero().as_tuple()
-        espaloma_bonds[(i, j)] = k
+def compare_bonds() -> Tuple[dict[str, list[float]], dict[str, float]]:
+    """Compare bond paramters assigned by ff and espaloma.
 
-    assert espaloma_bonds.keys() == sage_bonds.keys()
+    Returns a map of smirks->[espaloma values], and a map of smirks->sage_value
+    """
+    sage_values = {}
+    # map of smirks -> disagreement count
+    diffs = defaultdict(list)
+    for mol in tqdm(
+        itertools.islice(molecules(ds), None),
+        desc="Comparing bonds",
+        total=total,
+    ):
+        labels = ff.label_molecules(mol.to_topology())[0]
+        # angles = labels["Angles"]
+        # torsions = labels["ProperTorsions"]
 
-    if verbose:
-        print(f"{'i':>5}{'j':>5}{'Sage':>12}{'Espaloma':>12}{'Diff':>12}")
-    for k, v in sage_bonds.items():
-        v, smirks = v
-        diff = abs(v - espaloma_bonds[k])
-        i, j = k
-        if diff > EPS:
-            if verbose:
-                print(f"{i:5}{j:5}{v:12.8}{espaloma_bonds[k]:12.8}{diff:12.8}")
-            diffs[smirks].append(espaloma_bonds[k])
-            sage_values[smirks] = v
+        bonds = labels["Bonds"]
+        sage_bonds = {}
+        for k, v in bonds.items():
+            i, j = k
+            # t = (i, j, v.length.magnitude, v.k.magnitude)
+            sage_bonds[(i, j)] = (v.k.magnitude, v.smirks)
+
+        _, d = espaloma_label(mol)
+        espaloma = {}
+        for bond in d["bonds"]:
+            i, j, _, k = bond.from_zero().as_tuple()
+            espaloma[(i, j)] = k
+
+        assert espaloma.keys() == sage_bonds.keys()
+
+        if verbose:
+            print(f"{'i':>5}{'j':>5}{'Sage':>12}{'Espaloma':>12}{'Diff':>12}")
+        for k, v in sage_bonds.items():
+            v, smirks = v
+            diff = abs(v - espaloma[k])
+            i, j = k
+            if diff > EPS:
+                if verbose:
+                    print(f"{i:5}{j:5}{v:12.8}{espaloma[k]:12.8}{diff:12.8}")
+                diffs[smirks].append(espaloma[k])
+                sage_values[smirks] = v
+
+    return diffs, sage_values
+
+
+def print_summary(diffs, sage_values, outfile=None):
+    """Print a summary of diffs and sage_values to `outfile` or stdout if None.
+
+    The output format is `SMIRKS Count Sage Rest`, where Rest is all of the
+    espaloma values for a given SMIRKS pattern
+    """
+    needs_close = False
+    if outfile is None:
+        outfile = sys.stdout
+    elif isinstance(outfile, str):
+        outfile = open(outfile, "w")
+        needs_close = True
+
+    print("# Difference Summary", file=outfile)
+    # compute the max len of smirks patterns for pretty printing
+    ml = max([len(s) for s in diffs.keys()])
+    print(
+        f"# {'SMIRKS':<{ml - 2}}{'Count':>5}{'Sage':>8}{'Rest':>8}",
+        file=outfile,
+    )
+    items = [pair for pair in diffs.items()]
+    items.sort(key=lambda x: len(x[1]), reverse=True)
+    for smirks, values in items:
+        count = len(values)
+        print(
+            f"{smirks:{ml}}{count:5}{sage_values[smirks]:8.2f}",
+            end="",
+            file=outfile,
+        )
+        for v in values:
+            print(f"{v:8.2f}", end="", file=outfile)
+        print(file=outfile)
+
+    if needs_close:
+        outfile.close()
 
 
 # counting occurences of disagreement is somewhat interesting, but more useful
@@ -72,16 +120,5 @@ for mol in tqdm(
 # parameter toward the average espaloma value. or if espaloma has an especially
 # large range of values, that would be an indicator that we need to break up
 # one of our parameters
-
-print("# Difference Summary")
-# compute the max len of smirks patterns for pretty printing
-ml = max([len(s) for s in diffs.keys()])
-print(f"# {'SMIRKS':<{ml - 2}}{'Count':>5}{'Sage':>8}{'Rest':>8}")
-items = [pair for pair in diffs.items()]
-items.sort(key=lambda x: len(x[1]), reverse=True)
-for smirks, values in items:
-    count = len(values)
-    print(f"{smirks:{ml}}{count:5}{sage_values[smirks]:8.2f}", end="")
-    for v in values:
-        print(f"{v:8.2f}", end="")
-    print()
+diffs, sage_values = compare_bonds()
+print_summary(diffs, sage_values, outfile="bonds.dat")
