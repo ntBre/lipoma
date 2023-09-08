22d94eb775917ea16fbb59309a8bf8a4c958b417
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-08 11:01:02 -0400

    use click

--- parse_query.py
+++ parse_query.py
@@ -1,20 +1,32 @@
 # read the results produced by query.py and generate useful graphics
 
+import click
 import matplotlib.pyplot as plt
-import seaborn as sea
 import numpy as np
+import seaborn as sea
 
-with open("query.dat", "r") as inp:
-    for line in inp:
-        sp = line.split()
-        [smirks, count, sage, *rest] = sp
-        assert int(count) == len(rest)
-        data = [float(r) for r in rest]
-        ax = sea.histplot(data=data, label="Espaloma")
-        esp_avg = np.average(data)
-        ax.axvline(x=float(sage), color="green", label="Sage")
-        ax.axvline(x=esp_avg, color="orange", label="Espaloma Avg.")
-        fig = ax.get_figure()
-        plt.legend()
 
-        fig.savefig("out.png", dpi=300)
+@click.command()
+@click.option("--infile")
+def main(infile):
+    with open(infile, "r") as inp:
+        for i, line in enumerate(inp):
+            if line.startswith("#"):
+                continue
+            sp = line.split()
+            [smirks, count, sage, *rest] = sp
+            assert int(count) == len(rest)
+            data = [float(r) for r in rest]
+            ax = sea.histplot(data=data, label="Espaloma")
+            esp_avg = np.average(data)
+            ax.axvline(x=float(sage), color="green", label="Sage")
+            ax.axvline(x=esp_avg, color="orange", label="Espaloma Avg.")
+            fig = ax.get_figure()
+            plt.legend()
+
+            fig.savefig(f"output/{i:05d}.png", dpi=300)
+            plt.close()
+
+
+if __name__ == "__main__":
+    main()
