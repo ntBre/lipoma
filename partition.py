49e195b4b1e8e5df91af68d8cf69231439669d5c
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-19 09:42:10 -0400

    tidy up partition, finally get it to work in the repl

--- partition.py
+++ partition.py
@@ -1,11 +1,13 @@
 # like cluster, but instead of focusing on the "outliers" also check the main
 # group
 
-from openff.qcsubmit.results import OptimizationResultCollection
-from tqdm import tqdm
-from vflib.utils import Timer
+from collections import defaultdict
 
-from cluster import load_thresh
+from openff.qcsubmit.results import OptimizationResultCollection
+from openff.toolkit import Molecule
+from tqdm import tqdm
+
+from cluster import deduplicate_by, load_thresh
 from main import espaloma_label
 
 
@@ -14,20 +16,20 @@ def partition():
 
     opt = OptimizationResultCollection.parse_file("datasets/filtered-opt.json")
 
-    # demand execution here so I can deduplicate and reuse molecules
-    molecules = [
-        m for m in tqdm(opt.to_molecules(), desc="Converting molecules")
-    ]
+    molecules = deduplicate_by(
+        tqdm(opt.to_molecules(), desc="Converting molecules"),
+        Molecule.to_inchikey,
+    )
 
-    timer = Timer()
-    # TODO make these entries in a dict over smirks
-
-    # molecules matching the fn returned by load_thresh
-    in_thresh = []
-    # molecules not matching the fn returned by load_thresh
-    out_thresh = []
+    # molecules matching and not matching the fn returned by load_thresh
+    in_thresh = defaultdict(list)
+    out_thresh = defaultdict(list)
     for s, (smirks, fn) in enumerate(smirkss):
-        for m, molecule in enumerate(molecules):
+        for m, molecule in tqdm(
+            enumerate(molecules),
+            total=len(molecules),
+            desc=f"Processing smirks {s}",
+        ):
             matches = molecule.chemical_environment_matches(smirks)
             if matches:
                 _, esp = espaloma_label(molecule)
@@ -37,10 +39,9 @@ def partition():
                     if (b.atom1 - 1, b.atom2 - 1) in matches and fn(b.k)
                 ]
                 if esp:
-                    in_thresh.append(molecule)
+                    in_thresh[smirks].append(molecule)
                 else:
-                    out_thresh.append(molecule)
-        timer.say(f"finished smirks {s}")
+                    out_thresh[smirks].append(molecule)
         break  # only checking b84 for now
 
     return in_thresh, out_thresh
