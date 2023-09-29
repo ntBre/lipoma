61ba801ce6c7f144d6db35ff0d7db45e7a3de673
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-29 13:06:51 -0400

    also show the espaloma average on the graph

--- board.py
+++ board.py
@@ -5,6 +5,7 @@ with warnings.catch_warnings():
 
     import base64
 
+    import numpy as np
     import plotly.express as px
     from dash import Dash, Input, Output, callback, dcc, html
     from openff.toolkit import Molecule
@@ -24,7 +25,14 @@ colors = {"background": "white", "text": "black"}
 fig = px.histogram(
     record.espaloma_values, title=f"{record.ident} {smirks}", labels="Espaloma"
 )
-fig.add_vline(x=record.sage_value, annotation_text="Sage", line_dash="dash")
+fig.add_vline(
+    x=record.sage_value, annotation_text="Sage Avg.", line_dash="dash"
+)
+fig.add_vline(
+    x=np.average(record.espaloma_values),
+    annotation_text="Espaloma Avg.",
+    line_dash="dash",
+)
 fig.update_traces(marker_line_width=1, name="Espaloma")
 
 app.layout = html.Div(
