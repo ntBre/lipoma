e2a1c9179deaf2f0cf51277108e3024b170103e7
Brent Westbrook <brentrwestbrook@gmail.com>
2023-10-13 13:47:05 -0400

    extract paramereter ids for graphs

--- parse_query.py
+++ parse_query.py
@@ -8,6 +8,7 @@ import click
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sea
+from openff.toolkit import ForceField
 
 # Usage:
 # python parse_query.py -i data/bonds_dedup.dat -o /tmp/bonds [-p]
@@ -21,6 +22,15 @@ def main(infile, outdir, plot):
     if os.path.exists(outdir):
         shutil.rmtree(outdir)
     os.makedirs(outdir)
+
+    ff = ForceField("openff-2.1.0.offxml")
+    bh = ff.get_parameter_handler("Bonds")
+    ah = ff.get_parameter_handler("Angles")
+    th = ff.get_parameter_handler("ProperTorsions")
+
+    pmap = {h.smirks: h.id for h in bh}
+    pmap.update({h.smirks: h.id for h in ah})
+    pmap.update({h.smirks: h.id for h in th})
 
     buf = StringIO()
     with open(infile, "r") as inp:
@@ -40,8 +50,9 @@ def main(infile, outdir, plot):
                 ax.axvline(x=float(sage), color="green", label="Sage")
                 ax.axvline(x=esp_avg, color="orange", label="Espaloma Avg.")
                 fig = ax.get_figure()
+                pid = pmap[smirks]
                 plt.legend()
-                plt.title(f"{smirks}\navg = {esp_avg}")
+                plt.title(f"{pid}: {smirks}\navg = {esp_avg}")
                 fig.savefig(f"{outdir}/{i:05d}.png", dpi=300)
                 plt.close()
 
