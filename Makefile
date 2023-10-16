ba14d4cd22d245158effdc5d85d3789bf15ad408
Brent Westbrook <brentrwestbrook@gmail.com>
2023-10-16 13:16:58 -0400

    remove finished notes and add make recipe for calling query

--- Makefile
+++ Makefile
@@ -7,3 +7,9 @@ industry:
 	$(call parse_query,bonds)
 	$(call parse_query,angles)
 	$(call parse_query,torsions)
+
+json := $(addprefix data/industry/,bonds_dedup.json angles_dedup.json	\
+				   torsions_dedup.json)
+$(json): query.py
+	python query.py --dataset ../benchmarking/datasets/industry.json \
+			--out-dir data/industry
--- query.py
+++ query.py
@@ -327,13 +327,3 @@ def main(force_constants, dataset, out_dir):
 
 if __name__ == "__main__":
     main()
-
-# usage for industry data set:
-# python query.py -d ../benchmarking/datasets/industry.json -o data/industry
-
-# Now that I have the dashboard running, I see that separate bars in the
-# histogram can correspond to the same molecule, and my current drawing scheme
-# treats them the same way. Instead of calling chemical_environment_matches in
-# the plotting code, I should capture the Espaloma matches here and store it in
-# the Record. Then I can visualize the exact pattern the Espaloma value
-# corresponds to
